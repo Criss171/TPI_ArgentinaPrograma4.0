5565c28fa4f2309795361f92abab822a4e140877
Criss171 <44389803+Criss171@users.noreply.github.com>
2023-04-19 02:21:09 -0300

    Clases base definidas

---
+++ TPI/src/main/java/com/mycompany/tpi/Equipo.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tpi;
+
+/**
+ *
+ * @author C
+ */
+public class Equipo {
+    private String nombre;
+    private String descripcion;
+    
+    public Equipo(){
+        this.nombre = "";
+        this.descripcion = "";
+    }
+    public Equipo(String nombre, String descripcion){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+}
---
+++ TPI/src/main/java/com/mycompany/tpi/Pronostico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tpi;
+
+/**
+ *
+ * @author C
+ */
+public class Pronostico {
+    private Partido partido;
+    private Equipo equipo;
+    private Resultado restulado;
+    
+}
