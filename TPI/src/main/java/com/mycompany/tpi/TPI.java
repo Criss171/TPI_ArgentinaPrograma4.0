5f46dd787ecfbb00de9cc01627c8fd83ce0d3718
Criss171 <44389803+Criss171@users.noreply.github.com>
2023-04-19 17:22:00 -0300

    Update TPI.java

--- TPI/src/main/java/com/mycompany/tpi/TPI.java
+++ TPI/src/main/java/com/mycompany/tpi/TPI.java
@@ -12,6 +12,6 @@ package com.mycompany.tpi;
 public class TPI {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Prueba");
     }
 }
